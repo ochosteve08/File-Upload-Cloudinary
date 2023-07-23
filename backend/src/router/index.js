5601f42f5e1ba05fdc92043491a2ccf7602a38d2
ochosteve <ochosteve08@gmail.com>
2023-07-23 03:08:09 +0100

    complete server file functions and update error code on routes

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,3 +1,10 @@
 const router = require('./upload.router');
+const { error } = require("../lib-handler");
+const {Router} = require('express');
 
-module.exports = router;
+const apiRouter = Router();
+
+apiRouter.use("/upload",router);
+apiRouter.use("*", () => error.throwNotFound({ item: "Route" }));
+
+module.exports = apiRouter;
